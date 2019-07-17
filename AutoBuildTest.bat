35f2e90aca555d80c09179712c34d3ef7d640250
kanduri.deepika <Kanduri.Deepika@philips.com>
2019-07-17 10:03:24 +0530

    changed .bat

--- AutoBuildTest.bat
+++ AutoBuildTest.bat
@@ -1,5 +1,2 @@
-@ECHO OFF
-csc HelloWorld.cs
-
-HelloWorld.exe
-PAUSE
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe C:\Users\320052452\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one.csproj
+C:\Users\320052452\BootCamp_CaseStudy_one\BootCamp_CaseStudy_one\bin\Debug\BootCamp_CaseStudy_one.exe
